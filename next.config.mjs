92ac92f1441fc880d4e2eb48250d635bfd2daa16
anjolnepal <86286241+anjolnepal@users.noreply.github.com>
2026-09-26 19:53:19 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  experimental: {
+    turbopack: false,
+  },
 };
-
-export default nextConfig;
